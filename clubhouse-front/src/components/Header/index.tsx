7be71ce68c14a2455520ad7e1a2e51a838546475
Dg-Lopes <lopesdouglas.dev@gmail.com>
2021-05-30 22:13:02 -0300

    I'm live

---
+++ clubhouse-front/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./index.module.scss";
+
+
+export default function Header(): JSX.Element {
+	return (
+		<div className={styles.container}>
+      
+		</div>
+	);
+}
---
+++ clubhouse-front/src/pages/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./index.module.scss";
+import {AiFillGithub} from "react-icons/ai";
+
+export default function Home(): JSX.Element {
+	return (
+		<div className={styles.container}>
+			<title>Clubhouse | Login</title>
+			<div className={styles.content}>
+				<h1>🔥 Welcome to Clubhouse</h1>
+				<div className={styles.description}>
+					<p>
+						We're working hard to get Clubhouse ready for everyone! <br/>
+						While we wrap up the finishing touches, we're adding <br /> 
+						people gradually to make sure nothing breaks. :)
+					</p>
+					<br/>
+					<br/>
+					<br/>
+					<p>
+						Anyone can join with an invite from an existing user -- or <br/>
+						reserve your username and we'll text you if you have <br/>
+						a friend on the app who can let you in. We are so grateful <br/>
+						you're here and can't wait to have you join us! 🙏
+					</p>
+					<button type="submit">
+						<div><AiFillGithub size={30} /></div>	Login with Github
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
